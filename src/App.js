755fa69ec7ba5255f9670a502afe086b6544c0e0
IsEvrythngTkn <paulngoertzen@gmail.com>
2018-07-21 17:23:20 -0700

    Adds form and a simple test to ensure it renders

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FeedingForm from './components/FeedingForm.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Duck Watcher</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container pt-4">
+          <div className="row align-items-center justify-content-center">
+            <div className="col-md-6">
+              <FeedingForm />      
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
